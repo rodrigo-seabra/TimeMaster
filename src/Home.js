c5f2ffd1e547c2a503fd47cbe6771826b6fca040
rodrigo-seabra <rodrigo.seabra08@outlook.com>
2024-03-20 21:50:02 -0300

    Usando linear gradiente da expo na pagina home

--- src/Home.js
+++ src/Home.js
@@ -1,44 +1,75 @@
 import { Text, View, Image, StyleSheet } from "react-native";
 import { useState, useEffect, useLayoutEffect } from "react";
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation } from "@react-navigation/native";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+
+import { LinearGradient } from "expo-linear-gradient";
+
 export default function Home() {
-    const navigation = useNavigation();
-    useLayoutEffect(() => {
-        navigation.setOptions({
-            headerTitle: () => (
-                <Image
-                    source={require('../assets/photos/LogoInline.png')}
-                    style={{ width: 200, height: 70, marginLeft: 80 }}
-                />
-            ),
-        });
-    }, [navigation]);
-    return (
-        <View>
-            <Text>HOME</Text>
-            <View style={styles.topTasks}>
-                <View style={styles.topTasksEfects}>
-                </View>
-            </View>
-        </View>
-    )
+  const navigation = useNavigation();
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerTitle: () => (
+        <Image
+          source={require("../assets/photos/LogoInline.png")}
+          style={{ width: 200, height: 70, marginLeft: 80 }}
+        />
+      ),
+    });
+  }, [navigation]);
+  return (
+    <View style={styles.globalContainer}>
+      <View style={styles.topTasks}>
+        <LinearGradient
+          c
+          colors={[
+            "rgba(160, 69, 69, 1)",
+            "rgba(210, 84, 84, 1)",
+            "rgba(221, 111, 111, 1)",
+            "rgba(253, 153, 153, 1)",
+          ]}
+          style={styles.topTasksEfects}
+          start={{ x: 1, y: 0.5 }}
+          end={{ x: 0, y: 0.5 }}
+        >
+          <View style={styles.infos}>
+            <MaterialCommunityIcons name="fire" style={styles.fire} />
+            <Text style={styles.MaisImportante}>+ Importantes hoje</Text>
+          </View>
+        </LinearGradient>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    topTasksEfects: {
-        width: 300,
-        height: 100,
-        borderRadius: 5,
-        // Gradient background
-        backgroundColor: 'rgb(221, 111, 111 )',
-        backgroundImage: 'linear-gradient(290deg, rgba(160, 69, 69, 1) 0%, rgba(210, 84, 84, 1) 35%, rgba(221, 111, 111, 1) 55%, rgba(253, 153, 153, 1) 90%)',
-        // Fallback background color if linear gradient is not supported
-        fallback: {
-            backgroundColor: 'rgb(160, 69, 69)',
-        },
-    },
-    topTasks: {
-        alignItems: 'center',
-    }
+  topTasksEfects: {
+    width: 310,
+    height: 210,
+    borderRadius: 5,
+  },
+  infos: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  fire: {
+    fontSize: 50,
+    color: "rgba(160, 69, 69, 1)",
+  },
+  topTasks: {
+    alignItems: "center",
+  },
+  MaisImportante: {
+    color: "white",
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  globalContainer: {
+    width: "100%",
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
-
